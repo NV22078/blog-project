c16f646b77e585a5017916cef6a14f481e2b2a25
NV22078 <155225366+NV22078@users.noreply.github.com>
2024-03-27 16:16:40 +0000

    5 - Hero Section

---
+++ src/pages/home/HomePage.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import MainLayout from "../../components/MainLayout";
+import Hero from "../home/container/Hero";
+
+const HomePage = () => {
+  return(
+<MainLayout>
+  <Hero />
+</MainLayout>
+  );
+};
+
+export default HomePage;
