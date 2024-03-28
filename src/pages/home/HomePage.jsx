d8d7422bac11731b867e55daac797501c530472c
NV22078 <155225366+NV22078@users.noreply.github.com>
2024-03-28 06:28:54 +0000

    6 - Article card component

--- src/pages/home/HomePage.jsx
+++ src/pages/home/HomePage.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import MainLayout from "../../components/MainLayout";
+import Articles from "./container/Articles";
 import Hero from "../home/container/Hero";
 
 const HomePage = () => {
   return(
 <MainLayout>
   <Hero />
+  <Articles />
 </MainLayout>
   );
 };
